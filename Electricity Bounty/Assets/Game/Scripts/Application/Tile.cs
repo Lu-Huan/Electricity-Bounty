9396d00b4cc7854a776335af56cfaed4b77d893c
Lu-Huan <180356757@qq.com>
2018-10-14 12:49:29 +0800

    2018/10/14

---
+++ Electricity Bounty/Assets/Game/Scripts/Application/Tile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Tile
+{
+    public Vector3 Position;
+
+    public object Data; //格子所保存的数据
+
+    public bool IsPath;
+
+    public Tile(Vector3 pos)
+    {
+        Position = pos;
+        IsPath = false;
+        Data = null;
+    }
+   
+
+}
